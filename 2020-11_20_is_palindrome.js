2dffcc7a830f41580037748fe017552c6b1438d2
Trinh Gliedt <chuot2008@gmail.com>
2020-11-20 22:15:24 -0800

    Is Palindrome

---
+++ 2020-11_20_is_palindrome.js
@@ -0,0 +1,24 @@
+// page 68
+// Is Palindrome
+// Strings like "Able was I, ere I saw
+// Elba" or "Madam, I'm Adam" could be
+// considered palindromes , because (if we ignore
+// spaces, punctuation and capitalization) the letters
+// are the same from front and back.
+// Create a function that returns a boolean whether
+// the string is a strict palindrome. For "a x a" or
+// "racecar" , return true . Do not ignore spaces,
+// punctuation and capitalization: if given "Dud" or
+// "oho!" , return false .
+function isPalindrome(str){
+    let check = false;
+    let len = str.length
+    for (let i = 0; i<Math.floor(len/2); i++) {
+        if (str[i] === str[len-1-i]) {check = true;}
+        else {check = false;}
+    }
+    return check;
+}
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("Dad"));
+console.log(isPalindrome("oho!"));
---
+++ 2020-11_20_is_palindrome.py
@@ -0,0 +1,24 @@
+# page 68
+# Is Palindrome
+# Strings like "Able was I, ere I saw
+# Elba" or "Madam, I'm Adam" could be
+# considered palindromes , because (if we ignore
+# spaces, punctuation and capitalization) the letters
+# are the same from front and back.
+# Create a function that returns a boolean whether
+# the string is a strict palindrome. For "a x a" or
+# "racecar" , return true . Do not ignore spaces,
+# punctuation and capitalization: if given "Dud" or
+# "oho!" , return false .
+def isPalindrome(str):
+    check = False;
+    for i, v in enumerate(str):
+        if v == str[-1-i]:
+            check = True
+        else:
+            check = False
+    return check
+
+print(isPalindrome("racecar"))
+print(isPalindrome("Dad"))
+print(isPalindrome("oho!"))
