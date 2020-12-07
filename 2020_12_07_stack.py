8ed5b43d8a4b46a8cf6506392515acc9b29e7782
Trinh Gliedt <chuot2008@gmail.com>
2020-12-07 14:46:55 -0800

    12/7/20

---
+++ 2020_12_07_stack.py
@@ -0,0 +1,50 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+    def is_empty(self):
+        return self.items == []
+    def push(self, item):
+        self.items.insert(0, item)
+    def pop(self):
+        return self.items.pop(0)
+    def peek(self):
+        return self.items[0]
+    def size(self):
+        return len(self.items)
+    # s = Stack()
+    # s.push('hello')
+    # s.push('true')
+    # print(s.pop())
+
+def infix_to_postfix(infix_expr):
+    prec = {}
+    prec["*"] = 3
+    prec["/"] = 3
+    prec["+"] = 2
+    prec["-"] = 2
+    prec["("] = 1
+    op_stack = Stack()
+    postfix_list = []
+    token_list = infix_expr.split()
+
+    for token in token_list:
+        if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or token in "0123456789":
+            postfix_list.append(token)
+        elif token == '(':
+            op_stack.push(token)
+        elif token == ')':
+            top_token = op_stack.pop()
+            while top_token != '(':
+                postfix_list.append(top_token)
+                top_token = op_stack.pop()
+        else:
+            while (not op_stack.is_empty()) and (prec[op_stack.peek()] >= prec[token]):
+                postfix_list.append(op_stack.pop())
+            op_stack.push(token)
+
+    while not op_stack.is_empty():
+        postfix_list.append(op_stack.pop())
+    return " ".join(postfix_list)
+
+print(infix_to_postfix("A * B + C * D"))
+print(infix_to_postfix("( A + B ) * C - ( D - E ) * ( F + G )"))
