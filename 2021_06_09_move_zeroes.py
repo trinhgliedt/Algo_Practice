1280b9d3817e7a997161b14f849479dafeb52bb4
Trinh Gliedt <chuot2008@gmail.com>
2021-06-09 14:21:46 -0700

    move zeroes

--- 2021_06_09_move_zeroes.py
+++ 2021_06_09_move_zeroes.py
@@ -1,18 +1,23 @@
 # given an array of integers, write a function to move all 0's to the end while maintaining the relative order of the other elements
-def moveZeroes(arr):
-    # j is to keep track of non-zero elements
-    j = 0
-    # move the non-zero elements to the beginning of the array
-    for num in arr:
-        if num != 0:
-            arr[j] = num
-            j += 1
-        print(arr)
-    #  len - k is the number of zeros. Set all array items from k onwards to zero
-    for i in range(j, len(arr)):
-        arr[i] = 0
-    return arr
+from typing import List
 
 
+class Solution:
+    def moveZeroes(self, arr: List[int]):
+        # j is to keep track of non-zero elements
+        j = 0
+        # move the non-zero elements to the beginning of the array
+        for num in arr:
+            if num != 0:
+                arr[j] = num
+                j += 1
+            print(arr)
+        #  len - k is the number of zeros. Set all array items from k onwards to zero
+        for i in range(j, len(arr)):
+            arr[i] = 0
+        return arr
+
+
+s = Solution()
 arr1 = [5, 0, 6, 2, 0, 0, 7, 1]
-print(moveZeroes(arr1))
+print(s.moveZeroes(arr1))
