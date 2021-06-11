751e3cf2269f039fc0cb207138fddb614e1afe76
Trinh Gliedt <chuot2008@gmail.com>
2021-06-11 14:10:39 -0700

    single number

---
+++ 2021_06_11_single_number.py
@@ -0,0 +1,54 @@
+# https://leetcode.com/problems/single-number/
+# 136. Single Number
+# Easy
+
+# 6443
+
+# 207
+
+# Add to List
+
+# Share
+# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+
+# Example 1:
+
+# Input: nums = [2,2,1]
+# Output: 1
+# Example 2:
+
+# Input: nums = [4,1,2,1,2]
+# Output: 4
+# Example 3:
+
+# Input: nums = [1]
+# Output: 1
+
+
+# Constraints:
+
+# 1 <= nums.length <= 3 * 104
+# -3 * 104 <= nums[i] <= 3 * 104
+# Each element in the array appears twice except for one element which appears only once.
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        numsSet = set(nums)
+        sumSet = sum(numsSet)
+
+        return 2*sumSet-sum(nums)
+
+
+s = Solution()
+# print(s.singleNumber([4, 1, 2, 1, 2]))
+# print(s.singleNumber([2, 2, 1]))
+# print(s.singleNumber([1]))
+print(s.singleNumber([17, 12, 5, -6, 12, 4, 17, -5,
+      2, -3, 2, 4, 5, 16, -3, -4, 15, 15, -4, -5, -6]))
