58177eea7b792a698bd034567cb18dc0de2d78c0
Trinh Gliedt <chuot2008@gmail.com>
2021-06-25 13:04:46 -0700

    Undrected_Graph_adjacency_matrix

--- 2021_06_25_Undrected_Graph_adjacency_matrix.py
+++ 2021_06_25_Undrected_Graph_adjacency_matrix.py
@@ -11,6 +11,7 @@ class Graph:
     def insertEdge(self, v1, v2):
         if self.withInBounds(v1, v2):
             self.graph[v1][v2] = 1
+            self.graph[v2][v1] = 1
 
     def printGraph(self):
         for i in range(self.numberOfNodes):
