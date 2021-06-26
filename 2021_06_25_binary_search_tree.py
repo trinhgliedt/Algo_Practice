025358ba66323d74d7895cc9dcc587527ce0d328
Trinh Gliedt <chuot2008@gmail.com>
2021-06-26 00:10:42 -0700

    binary search tree

--- 2021_06_25_binary_search_tree.py
+++ 2021_06_25_binary_search_tree.py
@@ -18,13 +18,36 @@ def insert(root, node):
     if root is None:
         root = node
         return
-    if root.data < root.data:
+    if root.data < node.data:
         if root.right is None:
             root.right = node
+        else:
+            insert(root.right, node)
+    else:
+        if root.left is None:
+            root.left = node
+        else:
+            insert(root.left, node)
 
 
-root = Node(4)
+def preorder(node):
+    if node is not None:
+        print(node.data, end=" ")
+        preorder(node.left)
+        preorder(node.right)
 
-root.left = Node(5)
-root.right = Node(6)
-root.left.left = Node(7)
+
+tree = Node(5)
+#	         5
+#       /  	      \
+#     3	            7
+#   /   \        /     \
+#  2     4      6        8
+insert(tree, Node(3))
+insert(tree, Node(2))
+insert(tree, Node(4))
+insert(tree, Node(7))
+insert(tree, Node(6))
+insert(tree, Node(8))
+
+preorder(tree)
