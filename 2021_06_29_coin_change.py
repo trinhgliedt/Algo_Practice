d65c4987e61096af1689d6a6c0d82b996ddefd6f
Trinh Gliedt <chuot2008@gmail.com>
2021-06-30 14:39:06 -0700

    unique-paths

--- 2021_06_29_coin_change.py
+++ 2021_06_29_coin_change.py
@@ -47,14 +47,15 @@ class Solution:
             return -1
 
         INT_MAX = 1 << 32
-
         dp = [INT_MAX] * (amount+1)
+        print(dp)
         dp[0] = 0
         for i in range(1, amount + 1):
             for coin in coins:
                 if coin <= i:
                     dp[i] = min((dp[i-coin] + 1), dp[i])
 
+        print(dp)
         return dp[amount] if dp[amount] != INT_MAX else -1
 
 
