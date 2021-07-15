80ebccc767f538ede1f339b9b4c5a422b7c63991
Trinh Gliedt <chuot2008@gmail.com>
2021-07-14 22:39:53 -0700

    word search, with comments

--- 2021_06_19_word-search.py
+++ 2021_06_19_word-search.py
@@ -33,25 +33,32 @@
 
 # Follow up: Could you use search pruning to make your solution faster with a larger board?
 class Solution:
+    # dx represents the change of positions that we can go to for axis
     dx = [0, 0, -1, 1]
+    # dy represents the change of positions that we can go to for y axis
     dy = [1, -1, 0, 0]
 
     def solution(self, board, word, x, y, cur):
+        # if the current position is out of bounds of the board, or if current char is visited, return False
         if(x < 0 or x >= len(board) or y < 0 or y >= len(board[x]) or board[x][y] == ' '):
             return False
         cur += board[x][y]
-
+        #  if current string is longer than the word, return False
         if(len(cur) > len(word)):
             return False
+        # if the last character of the current string is different than character of the word at the same position, return False
         if(cur[len(cur)-1] != word[len(cur)-1]):
             return False
         if(cur == word):
             return True
-
+        # save the current value of board[x][y] to a variable and assign another value to board[x][y] to indicate it's visited
         temp = board[x][y]
+        # empty char represents a cell that has been visited
         board[x][y] = ' '
 
+        # 4 is the number of neighboring cells, vertical (up, down), and horizontal (left, right)
         for i in range(4):
+            # inside the loop, check if the result of recursively calling the solution function with the same set of params except this time, we would change the x, y positions to indicate visiting neighbors
             if(self.solution(board, word, x+self.dx[i], y+self.dy[i], cur)):
                 return True
 
@@ -62,9 +69,11 @@ class Solution:
         if(len(word) == 0):
             return True
         n = len(board)
+        # loop through the board to find a match for the first character of word
         for i in range(n):
             m = len(board[i])
             for j in range(m):
+                # start with an empty current string
                 if(word[0] == board[i][j] and self.solution(board, word, i, j, "")):
                     return True
         return False
